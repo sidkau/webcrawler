3cf8d93f95f4611d01e803e75027ff40033931f6
Siddharth K <siddharth.k@redbus.com>
2017-09-14 14:27:52 +0530

    first commit

--- crawler.js
+++ crawler.js
@@ -1,76 +1,31 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var URL = require('url-parse');
+var csvWriter = require('csv-write-stream')
+var fs = require('fs');
 
-var START_URL = "http://www.arstechnica.com";
-var SEARCH_WORD = "finds";
-var MAX_PAGES_TO_VISIT = 10;
-
-var pagesVisited = {};
-var numPagesVisited = 0;
-var pagesToVisit = [];
-var url = new URL(START_URL);
-var baseUrl = url.protocol + "//" + url.hostname;
-
-pagesToVisit.push(START_URL);
-crawl();
-
-function crawl() {
-
-  if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
-    console.log("Reached max limit of number of pages to visit.");
-    return;
-  }
-  var nextPage = pagesToVisit.pop();
-  if (nextPage in pagesVisited) {
-
-    // We've already visited this page, so repeat the crawl
-    crawl();
-  } else {
-    
-    // New page we haven't visited
-    visitPage(nextPage, crawl);
-  }
-}
-
-function visitPage(url, callback) {
-  // Add page to our set
-  pagesVisited[url] = true;
-  numPagesVisited++;
-
-  // Make the request
-  console.log("Visiting page " + url);
-  request(url, function(error, response, body) {
+var url = 'https://medium.com';
+var jsonUrl=[];
+request(url, function(err, resp, body){
+  $ = cheerio.load(body);
+  links = $('a'); //jquery get all hyperlinks
+  $(links).each(function(i, link){
+      jsonUrl.push($(link).attr('href'))
   
-     // Check status code (200 is HTTP OK)
-     console.log("Status code: " + response.statusCode);
-     if(response.statusCode !== 200) {
-       
-       callback();
-       return;
-     }
-     // Parse the document body
-     var $ = cheerio.load(body);
-     var isWordFound = searchForWord($, SEARCH_WORD);
-     if(isWordFound) {
-       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
-     } else {
-       collectInternalLinks($);
-       // In this short program, our callback is just calling crawl()
-       callback();
-     }
+    
   });
-}
 
-function searchForWord($, word) {
-  var bodyText = $('html > body').text().toLowerCase();
-  return(bodyText.indexOf(word.toLowerCase()) !== -1);
+//this is to convert json to csv
+  csvdata="";
+  seperator=";";
+  nl="\n";
+  for(a in jsonUrl){
+      csvdata+=jsonUrl[a]+nl;
+  }
+  fs.writeFile("crawler.csv",csvdata,"utf-8",function(err){
+if(err){
+console.log("error"+err)
+}else{
+  console.log("completed")
 }
-
-function collectInternalLinks($) {
-    var relativeLinks = $("a[href^='/']");
-    console.log("Found " + relativeLinks.length + " relative links on page");
-    relativeLinks.each(function() {
-        pagesToVisit.push(baseUrl + $(this).attr('href'));
-    });
-}
+});
+});
